055f7b57837fb61a8c1193d1d11c059b740db60c
yerrouhamza <hamzayerrou5@yahoo.com>
2025-02-12 13:14:46 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import ComingSoon from "./components/comingSoon";
+
+function App() {
+  return <ComingSoon />
+}
+
+export default App;
---
+++ src/components/comingSoon.tsx
@@ -0,0 +1,7 @@
+export default function ComingSoon() {
+  return (
+    <div>
+      Coming Soon
+    </div>
+  );
+};
